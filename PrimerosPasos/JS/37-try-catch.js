3a24a9f2e18740f8cb4e624f5a9147d54d3ac4e9
Carlos Quezada <quezadacarlos2031@gmail.com>
2023-10-13 16:08:18 -0500

    Fecth

---
+++ PrimerosPasos/JS/37-try-catch.js
@@ -0,0 +1,8 @@
+'use strict'
+
+try{
+    var year = 2023;
+    alert(yea);
+}catch(error){
+    console.log(error);
+}
---
+++ PrimerosPasos/JS/38-fechas.js
@@ -0,0 +1,9 @@
+var fecha = new Date();
+
+var textoHora = `
+    El año es ${fecha.getFullYear()},
+    El mes es ${fecha.getMonth()},
+    El día es ${fecha.getDate()}
+`
+
+console.log(textoHora);
